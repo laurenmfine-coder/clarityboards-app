e35c4d4a194d0bcc10846f66ac7585e6d54c75f1
laurenmfine-coder <laurenmfine@gmail.com>
2026-03-10 21:44:12 -0400

    Fix TypeScript type error in gcal route

--- app/api/gcal/route.ts
+++ app/api/gcal/route.ts
@@ -54,7 +54,7 @@ function makeSupabaseClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get: (name) => cookieStore.get(name)?.value,
+        get: (name: string) => cookieStore.get(name)?.value,
         set: () => {},
         remove: () => {},
       },
