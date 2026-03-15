0afca3061c256957daa18b98b12369f65df67c63
laurenmfine-coder <laurenmfine@gmail.com>
2026-03-15 19:46:22 -0400

    feat: add Google Analytics (G-DPKYH8YWG5)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata, Viewport } from 'next'
+﻿import type { Metadata, Viewport } from 'next'
 import './globals.css'
 import { ToastProvider } from '@/components/ToastProvider'
 import { NextIntlClientProvider } from 'next-intl'
